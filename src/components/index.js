2720d516ff623bff44fadf1e160dae62db55c8b6
MohitMorya2 <mohitmorya56@gmail.com>
2025-01-08 18:59:07 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container"
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import SignUp from "./SignUp";
+import Login from "./Login";
+import AuthLayout from './AuthLayout';
+import PostForm from "./post-form/PostForm";
+import Select from './Select';
+import PostCard from './PostCard';
+import Button from './Button';
+import Input from './Input'
+
+
+export{Header, Footer, Container, Logo, LogoutBtn, RTE, SignUp, Login, AuthLayout, PostForm, Select, PostCard, Button, Input};
